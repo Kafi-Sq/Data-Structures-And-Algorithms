0d91cde639eec363709082004f32f907325185c8
Hassen Hassen <xasansq@gmail.com>
2021-08-30 14:38:31 -0500

    Added Sorting

---
+++ Sorting/index.js
@@ -0,0 +1,8 @@
+/* Elementary Sorting Algorithms
+    - Sorting is the process of rearranging the items in a collection
+    (eg. an array) so that the items are in some sort of order.
+        Examples
+    Sorting numbers from smallest to largest
+    Sorting names alphabetically
+    Sorting movies based on release year
+*/
